2d02420f740c6bc57f9e9007a54480f7477eaf3c
elizabeth <jelizabethpardo@gmail.com>
2018-09-29 17:26:01 -0300

    elizabet

--- clinicaveterinaria (1).sql
+++ clinicaveterinaria (1).sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 29-09-2018 a las 22:20:12
+-- Tiempo de generación: 29-09-2018 a las 22:25:26
 -- Versión del servidor: 10.1.35-MariaDB
 -- Versión de PHP: 7.2.9
 
