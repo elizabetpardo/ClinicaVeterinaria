f244b174f12d27a4581eb9d38ddb30707a6f29ad
Yanina Paredes <yaninaparedes2018@gmail.com>
2018-09-29 15:21:26 -0300

    yanina

---
+++ ACTUALIZAR DATOS DEL PROYECTO.bat
@@ -0,0 +1,3 @@
+cd C:\Users\usuario\Documents\NetBeansProjects\ClinicaVeterinaria
+git pull https://github.com/elizabetpardo/ClinicaVeterinaria.git
+pause
---
+++ SUBIR DATOS AL PROYECTO.bat
@@ -0,0 +1,5 @@
+cd C:\Users\usuario\Documents\NetBeansProjects\ClinicaVeterinaria
+git add .
+git commit -m "yanina"
+git push -u origin master
+pause
