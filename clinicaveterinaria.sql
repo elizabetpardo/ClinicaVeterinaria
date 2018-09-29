d27570ac99baeb97eabf11d0626f6035235adf34
elizabeth <jelizabethpardo@gmail.com>
2018-09-29 17:22:37 -0300

    elizabet

---
+++ clinicaveterinaria.sql
@@ -0,0 +1,169 @@
+-- phpMyAdmin SQL Dump
+-- version 4.8.3
+-- https://www.phpmyadmin.net/
+--
+-- Servidor: 127.0.0.1
+-- Tiempo de generación: 29-09-2018 a las 22:20:12
+-- Versión del servidor: 10.1.35-MariaDB
+-- Versión de PHP: 7.2.9
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de datos: `clinicaveterinaria`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `cliente`
+--
+
+CREATE TABLE `cliente` (
+  `id_cliente` int(11) NOT NULL,
+  `dni` int(11) NOT NULL,
+  `apellido` varchar(50) COLLATE latin1_spanish_ci NOT NULL,
+  `nombre` varchar(50) COLLATE latin1_spanish_ci NOT NULL,
+  `direccion` varchar(50) COLLATE latin1_spanish_ci NOT NULL,
+  `telefono` varchar(20) COLLATE latin1_spanish_ci NOT NULL,
+  `persona_alternativa` varchar(50) COLLATE latin1_spanish_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_spanish_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `mascota`
+--
+
+CREATE TABLE `mascota` (
+  `id_mascota` int(11) NOT NULL,
+  `id_cliente` int(11) NOT NULL,
+  `cod_mascota` varchar(20) COLLATE latin1_spanish_ci NOT NULL,
+  `alias` varchar(20) COLLATE latin1_spanish_ci NOT NULL,
+  `sexo` varchar(20) COLLATE latin1_spanish_ci NOT NULL,
+  `especie` varchar(30) COLLATE latin1_spanish_ci NOT NULL,
+  `raza` varchar(30) COLLATE latin1_spanish_ci NOT NULL,
+  `color_pelo` varchar(20) COLLATE latin1_spanish_ci NOT NULL,
+  `fecha_nac` date NOT NULL,
+  `peso_promedio` double NOT NULL,
+  `peso_actual` double NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_spanish_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `tratamiento`
+--
+
+CREATE TABLE `tratamiento` (
+  `id_tratamiento` int(11) NOT NULL,
+  `cod_tratamiento` varchar(20) COLLATE latin1_spanish_ci NOT NULL,
+  `tipo` int(11) NOT NULL,
+  `descripcion` varchar(300) COLLATE latin1_spanish_ci NOT NULL,
+  `importe` double NOT NULL,
+  `activo` tinyint(1) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_spanish_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `visitadeatencion`
+--
+
+CREATE TABLE `visitadeatencion` (
+  `id_visita` int(11) NOT NULL,
+  `id_mascota` int(11) NOT NULL,
+  `id_tratamiento` int(11) NOT NULL,
+  `fecha` date NOT NULL,
+  `detalle` varchar(150) COLLATE latin1_spanish_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_spanish_ci;
+
+--
+-- Índices para tablas volcadas
+--
+
+--
+-- Indices de la tabla `cliente`
+--
+ALTER TABLE `cliente`
+  ADD PRIMARY KEY (`id_cliente`);
+
+--
+-- Indices de la tabla `mascota`
+--
+ALTER TABLE `mascota`
+  ADD PRIMARY KEY (`id_mascota`),
+  ADD KEY `id_cliente` (`id_cliente`);
+
+--
+-- Indices de la tabla `tratamiento`
+--
+ALTER TABLE `tratamiento`
+  ADD PRIMARY KEY (`id_tratamiento`);
+
+--
+-- Indices de la tabla `visitadeatencion`
+--
+ALTER TABLE `visitadeatencion`
+  ADD PRIMARY KEY (`id_visita`),
+  ADD KEY `id_mascota` (`id_mascota`),
+  ADD KEY `id_tratamiento` (`id_tratamiento`);
+
+--
+-- AUTO_INCREMENT de las tablas volcadas
+--
+
+--
+-- AUTO_INCREMENT de la tabla `cliente`
+--
+ALTER TABLE `cliente`
+  MODIFY `id_cliente` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `mascota`
+--
+ALTER TABLE `mascota`
+  MODIFY `id_mascota` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `tratamiento`
+--
+ALTER TABLE `tratamiento`
+  MODIFY `id_tratamiento` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `visitadeatencion`
+--
+ALTER TABLE `visitadeatencion`
+  MODIFY `id_visita` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- Restricciones para tablas volcadas
+--
+
+--
+-- Filtros para la tabla `mascota`
+--
+ALTER TABLE `mascota`
+  ADD CONSTRAINT `mascota_ibfk_1` FOREIGN KEY (`id_cliente`) REFERENCES `cliente` (`id_cliente`);
+
+--
+-- Filtros para la tabla `visitadeatencion`
+--
+ALTER TABLE `visitadeatencion`
+  ADD CONSTRAINT `visitadeatencion_ibfk_1` FOREIGN KEY (`id_tratamiento`) REFERENCES `tratamiento` (`id_tratamiento`),
+  ADD CONSTRAINT `visitadeatencion_ibfk_2` FOREIGN KEY (`id_mascota`) REFERENCES `mascota` (`id_mascota`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
