cf5e0cc185e5f60e85e89920ce9926af9daae017
Yanina Paredes <yaninaparedes2018@gmail.com>
2018-10-03 19:26:16 -0300

    yanina

--- src/clinicaveterinaria/Modelo/ClienteData.java
+++ src/clinicaveterinaria/Modelo/ClienteData.java
@@ -87,6 +87,93 @@ public class ClienteData {
         
         return clientes;
     }
+    public void borrarCliente(int id){
+    try {
+            
+            String sql = "DELETE FROM cliente WHERE id =?;";
+
+            PreparedStatement statement = connection.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS);
+            statement.setInt(1, id);
+           
+            
+            statement.executeUpdate();
+            
+            
+            statement.close();
+    
+        } catch (SQLException ex) {
+            System.out.println("Error al insertar un cliente: " + ex.getMessage());
+        }
+        
+    
+    }
+    
+    public void actualizarCliente(Cliente cliente){
+    
+        try {
+            
+            String sql = "UPDATE alumno SET nombre = ?, fecNac = ? , activo =? WHERE id = ?;";
+
+            PreparedStatement statement = connection.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS);
+            
+            statement.setInt(1, cliente.getDni());
+            statement.setString(2, cliente.getApellido());
+            statement.setString(3, cliente.getNombre());
+            statement.setString(4, cliente.getDireccion());
+            statement.setString(5, cliente.getTelefono());
+            statement.setString(6, cliente.getPersona_alternativa());
+            statement.executeUpdate();
+            
+          
+            statement.close();
+    
+        } catch (SQLException ex) {
+            System.out.println("Error al insertar un alumno: " + ex.getMessage());
+        }
+    
+    }
+    
+    public Cliente buscarCliente(int id){
+    Cliente cliente=null;
+    try {
+            
+            String sql = "SELECT * FROM cliente WHERE id =?;";
+
+            PreparedStatement statement = connection.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS);
+            statement.setInt(1, id);
+           
+            
+            ResultSet resultSet=statement.executeQuery();
+            
+            while(resultSet.next()){
+                cliente = new Cliente();
+                
+                    cliente.setId_cliente(resultSet.getInt("id_cliente"));
+                    cliente.setDni(resultSet.getInt("dni"));
+                    cliente.setApellido(resultSet.getString("apellido"));
+                    cliente.setNombre(resultSet.getString("nombre"));
+                    cliente.setDireccion(resultSet.getString("direccion"));
+                    cliente.setTelefono(resultSet.getString("telefono"));
+                    cliente.setPersona_alternativa(resultSet.getString("persona alternativa"));
+                       
+                
+            }      
+            statement.close();
+            
+            
+            
+            
+    
+        } catch (SQLException ex) {
+            System.out.println("Error al insertar un cliente: " + ex.getMessage());
+        }
+        
+        return cliente;
+    }
+    
+
+    
+    
     
     
 }
